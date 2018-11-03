6c415e33f0870069ffdf0c74766df5b47d0636bc
Abhi160a <43521937+Abhi160a@users.noreply.github.com>
2018-11-03 10:43:42 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,12 +1,12 @@
 def reverse(h):
-    str=' '
+    str=''
     for i in h:
         str = i + str
         return str
     a=input("Enter a string")
-    print("The string is:",end=" ")
+    print("The string is:",end="")
     print(a)
-    print("The reverse string is:",end=" ")
+    print("The reverse string is:",end="")
     print(reverse(a))
 
 
