f81f5da32a3fa6a4318530f2796dad62b7faa8f5
Abhi160a <43521937+Abhi160a@users.noreply.github.com>
2018-11-03 10:38:38 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,16 @@
+def reverse(h):
+    str=' '
+    for i in h:
+        str = i + str
+        return str
+    a=input("Enter a string")
+    print("The string is:",end=" ")
+    print(a)
+    print("The reverse string is:",end=" ")
+    print(reverse(a))
+
+
+
+
+
+    
